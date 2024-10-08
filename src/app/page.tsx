c12fb87d192fe8d8cc038eab34a1f47f18a34f4f
Gabriel Cumare <gacm95218@gmail.com>
2024-10-07 20:58:47 -0700

    Installed Shadcn components and configurations to project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="justify-center">
+      <Button className="m-5 justify-center">Hi</Button>
+      </div>
+    </div>
+  );
+}
